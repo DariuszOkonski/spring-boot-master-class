e31975aa7d5c3b3440a994761d23f081ab36e3f8
Dariusz Okonski <darek200180@gmail.com>
2022-01-03 14:59:11 +0000

    add customer package

---
+++ src/main/java/com/example/springbootmasterclass/customer/CustomerRepository.java
@@ -0,0 +1,8 @@
+package com.example.springbootmasterclass.customer;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class CustomerRepository {
+
+}
---
+++ src/main/java/com/example/springbootmasterclass/customer/CustomerService.java
@@ -0,0 +1,8 @@
+package com.example.springbootmasterclass.customer;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerService {
+
+}
