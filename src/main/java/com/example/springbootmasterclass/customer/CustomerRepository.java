5c0f50cfb96554af1cb32be6d15d7e18ed772fce
Dariusz Okonski <darek200180@gmail.com>
2022-01-04 12:59:50 +0000

    add repository layer

--- src/main/java/com/example/springbootmasterclass/customer/CustomerRepository.java
+++ src/main/java/com/example/springbootmasterclass/customer/CustomerRepository.java
@@ -1,8 +1,22 @@
 package com.example.springbootmasterclass.customer;
 
-import org.springframework.stereotype.Repository;
 
-@Repository
-public class CustomerRepository {
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+@Component
+@Primary
+public class CustomerRepository implements CustomerRepo{
+
+    @Override
+    public List<Customer> getCustomers() {
+//        TODO connect to real db
+        return Collections.singletonList(
+                new Customer(1L, "TODO. Implement real db")
+        );
+    }
 }
--- src/main/java/com/example/springbootmasterclass/customer/CustomerService.java
+++ src/main/java/com/example/springbootmasterclass/customer/CustomerService.java
@@ -1,11 +1,25 @@
 package com.example.springbootmasterclass.customer;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CustomerService {
 
-    public Customer getCustomer() {
-        return new Customer(1L, "James Bond");
+    private final CustomerRepo customerRepo;
+
+    @Autowired
+    public CustomerService(CustomerRepo customerRepo) {
+        this.customerRepo = customerRepo;
+    }
+
+    public List<Customer> getCustomers() {
+        return this.customerRepo.getCustomers();
     }
 }
+
+
+// public CustomerService(@Qualifier("fake") CustomerRepo customerRepo)
