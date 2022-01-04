b41f67b7c5f480e0dbda6ed86b4975ff20206e37
Dariusz Okonski <darek200180@gmail.com>
2022-01-04 13:08:17 +0000

    add more specific adnotations to Beans

--- src/main/java/com/example/springbootmasterclass/customer/CustomerService.java
+++ src/main/java/com/example/springbootmasterclass/customer/CustomerService.java
@@ -1,14 +1,12 @@
 package com.example.springbootmasterclass.customer;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class CustomerService {
-
     private final CustomerRepo customerRepo;
 
     @Autowired
