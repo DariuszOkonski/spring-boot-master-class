19e32aacf1375c99fb9f21de9829f3726877ab08
Dariusz Okonski <darek200180@gmail.com>
2022-01-05 19:32:21 +0000

    add post, put, delete

--- src/main/java/com/example/springbootmasterclass/customer/CustomerController.java
+++ src/main/java/com/example/springbootmasterclass/customer/CustomerController.java
@@ -1,8 +1,7 @@
 package com.example.springbootmasterclass.customer;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,8 +17,25 @@ public class CustomerController {
     }
 
     @GetMapping
-    List<Customer> getCustomers() {
-
+    public List<Customer> getCustomers() {
         return this.customerService.getCustomers();
     }
+
+    @PostMapping
+    public void createNewCustomer(@RequestBody Customer customer) {
+        System.out.println("POST REQUEST...");
+        System.out.println(customer);
+    }
+
+    @PutMapping()
+    public void updateCustomer(@RequestBody Customer customer) {
+        System.out.println("UpdateCustomer requeste");
+        System.out.println(customer);
+    }
+
+    @DeleteMapping(path = "{customerId}")
+    public void deleteCustomer(@PathVariable("customerId") Long id) {
+        System.out.println("DELETE Request for customer with id: " + id);
+    }
+
 }
