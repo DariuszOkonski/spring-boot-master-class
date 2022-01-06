cc0bcf34d09f31df2c8a0f4c7d2c9dda149307a2
Dariusz Okonski <darek200180@gmail.com>
2022-01-06 11:33:33 +0000

    requestmapping and api v2

--- src/main/java/com/example/springbootmasterclass/customer/CustomerControllerV2.java
+++ src/main/java/com/example/springbootmasterclass/customer/CustomerControllerV2.java
@@ -3,42 +3,41 @@ package com.example.springbootmasterclass.customer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
 import java.util.List;
 
-
-@RequestMapping(path = "api/v1/customer")
 @RestController
-@Deprecated
-public class CustomerController {
+@RequestMapping(path = "api/v2/customer")
+public class CustomerControllerV2 {
 
     private final CustomerService customerService;
 
     @Autowired
-    public CustomerController(CustomerService service) {
-
-        this.customerService = service;
+    public CustomerControllerV2(CustomerService customerService) {
+        this.customerService = customerService;
     }
 
     @GetMapping(value = "all")
-    public List<Customer> getCustomers() {
-
-        return this.customerService.getCustomers();
+    public List<Customer> getCustomer() {
+        return Arrays.asList(
+                new Customer(0L, "v2", "v2pass")
+        );
     }
 
     @PostMapping
     public void createNewCustomer(@RequestBody Customer customer) {
-        System.out.println("POST REQUEST...");
+        System.out.println("POST");
         System.out.println(customer);
     }
 
-    @PutMapping()
+    @PutMapping
     public void updateCustomer(@RequestBody Customer customer) {
-        System.out.println("PUT UpdateCustomer requeste");
+        System.out.println("PUT");
         System.out.println(customer);
     }
 
     @DeleteMapping(path = "{customerId}")
     public void deleteCustomer(@PathVariable("customerId") Long id) {
-        System.out.println("DELETE Request for customer with id: " + id);
+        System.out.println("DELETE: " + id);
     }
 }
