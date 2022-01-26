b9c40b116ede7bcdc131c59a2b537146231eb1cc
Dariusz Okonski <darek200180@gmail.com>
2022-01-26 14:44:14 +0000

    jsonignore and json property

--- src/main/java/com/example/springbootmasterclass/customer/CustomerFakeRepository.java
+++ src/main/java/com/example/springbootmasterclass/customer/CustomerFakeRepository.java
@@ -1,8 +1,5 @@
 package com.example.springbootmasterclass.customer;
 
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
-
 import java.util.Arrays;
 import java.util.List;
 
@@ -12,8 +9,8 @@ public class CustomerFakeRepository implements CustomerRepo{
     @Override
     public List<Customer> getCustomers() {
         return Arrays.asList(
-                new Customer(1L, "James Bond"),
-                new Customer(2L, "Jamila Ahmed")
+                new Customer(1L, "James Bond", "password123"),
+                new Customer(2L, "Jamila Ahmed", "123password")
         );
     }
 }
